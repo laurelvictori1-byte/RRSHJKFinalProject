9a60c470b6793696e86984129892a8c99cc4b29a
KCGO57 <kjc0765@gmail.com>
2026-05-26 13:50:40 -0700

    code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const goober = {
     height = 50,
     width = 50,
     speed = 10,
+    // jump here i dont know
 };
 
 const canvas = document.getElementById("canvas");
@@ -18,3 +19,17 @@ function addPlayer(){ // green square for now. will replace with actual player i
 let rightPress = false;
 let leftPress = false;
 
+function movePlayer(){
+    if(rightPress && player.x < canvas.width- player.width) {
+        player.x += player.speed;
+    }
+    if(leftPress && player.x > 0) {
+        player.x -= player.speed;
+    }
+}
+
+function gameLoop(){
+    movePlayer();
+    addPlayer();
+    requestAnimationFrame(gameLoop);
+}
