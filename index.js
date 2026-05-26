6eba8b4c58c783e44dfc8f1a3185ffd169247a50
KCGO57 <kjc0765@gmail.com>
2026-05-26 13:48:59 -0700

    code

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ const goober = {
     width = 50,
     speed = 10,
 };
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+
+function addPlayer(){ // green square for now. will replace with actual player image
+    ctx.fillStyle = "green";
+    ctx.fillRect(addPlayer.x, addPlayer.y, addPlayer.width, addPlayer.height)
+}
+
+let rightPress = false;
+let leftPress = false;
+
